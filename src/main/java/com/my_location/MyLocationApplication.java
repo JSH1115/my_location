fff34243b283bc5d53bb0ab2fec6df066ad4c662
정승헌 <rmaqhql@naver.com>
2023-01-08 22:53:38 +0900

    init

---
+++ src/main/java/com/my_location/MyLocationApplication.java
@@ -0,0 +1,14 @@
+package com.my_location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MyLocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my_location/MyLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
