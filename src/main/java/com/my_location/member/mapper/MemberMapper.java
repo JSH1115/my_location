cb5f135675eea93bc822bda40cf34b0c2238261e
정승헌 <rmaqhql@naver.com>
2023-01-09 00:18:39 +0900

    회원가입 mapper 세팅

--- src/main/java/com/my_location/member/mapper/MemberMapper.java
+++ src/main/java/com/my_location/member/mapper/MemberMapper.java
@@ -6,4 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
     MemberVO getMember(String id);
+
+    int insertMember(MemberVO memberVO);
 }
--- src/main/java/com/my_location/member/service/MemberService.java
+++ src/main/java/com/my_location/member/service/MemberService.java
@@ -2,11 +2,14 @@ package com.my_location.member.service;
 
 import com.my_location.member.mapper.MemberMapper;
 import com.my_location.member.vo.MemberVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberService {
 
+    @Autowired
     MemberMapper memberMapper;
 
     public MemberService(MemberMapper memberMapper) {
@@ -15,5 +18,11 @@ public class MemberService {
 
     public MemberVO getMember(String id) {
         return memberMapper.getMember(id);
+    }
+
+    @Transactional
+    public int insertMember(MemberVO memberVO) {
+        memberVO.setReg_ip("127.0.0.1");
+        return memberMapper.insertMember(memberVO);
     }
 }
