89125def60025c8a09ba96497f2db3f41513370e
정승헌 <rmaqhql@naver.com>
2023-01-09 23:29:02 +0900

    로그인 후 메인페이지로 이동 구현

--- src/main/java/com/my_location/member/controller/MemberController.java
+++ src/main/java/com/my_location/member/controller/MemberController.java
@@ -1,11 +1,17 @@
 package com.my_location.member.controller;
 
 import com.my_location.member.service.MemberService;
+import com.my_location.member.vo.MemberVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes("member")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -13,9 +19,29 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
-    @GetMapping("/login")
+    @GetMapping("/")
     public String signUpForm() {
         return "content/member/login";
     }
 
+    @GetMapping("/main")
+    public String main(@ModelAttribute("member") MemberVO memberVO) {
+        if (memberVO.getId() == null) {
+            return "redirect:/";
+        }
+
+        return "content/member/index";
+    }
+
+    @PostMapping("/login")
+    public String login(MemberVO memberVO, Model model) {
+        MemberVO findMember = memberService.getMember(memberVO.getId());
+
+        if (findMember != null && findMember.getPw().equals(memberVO.getPw())) {
+            model.addAttribute("member", findMember);
+            return "redirect:/main";
+        } else {
+            return "redirect:/";
+        }
+    }
 }
